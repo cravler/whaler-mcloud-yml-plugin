139d2deba8dfaabfaaf755a987e7bbff08332aff
Sergei Vizel <sergei.vizel@modera.net>
2018-05-18 09:15:43 +0300

    remove internal fs/promises usage

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
-const fs = require('fs/promises');
+const fs = require('fs');
+const util = require('util');
 const path = require('path');
+
+const fsReadFile = util.promisify(fs.readFile);
+const fsWriteFile = util.promisify(fs.writeFile);
+const fsUnlink = util.promisify(fs.unlink);
 
 module.exports = exports;
 
@@ -27,15 +32,15 @@ async function exports (whaler) {
 
         if (configFile && 'mcloud.yml' === path.basename(configFile)) {
             tmpFile = configFile + '.tmp';
-            let content = await fs.readFile(configFile, 'utf8');
+            let content = await fsReadFile(configFile, 'utf8');
             const yml = 'services:\n    ' + content.split('\n').join('\n    ');
-            await fs.writeFile(tmpFile, yml, 'utf8');
+            await fsWriteFile(tmpFile, yml, 'utf8');
         }
     });
 
     whaler.after('config', async ctx => {
         if (tmpFile) {
-            await fs.unlink(tmpFile);
+            await fsUnlink(tmpFile);
             tmpFile = null;
         }
     });
